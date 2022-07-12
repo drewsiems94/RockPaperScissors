2756bd6a93b721bff22679afa81b98f10f4b3b5f
Andrew Siems <siemsdrew@gmail.com>
2022-07-12 16:41:08 -0700

    Add function to generate a random choice of rock, paper, or scissors

---
+++ javascript.js
@@ -0,0 +1,10 @@
+function computerPlay() {
+    let play = Math.floor(Math.random() * 3);
+    if (play == 0) {
+        return "Rock";
+    } else if (play == 1) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
